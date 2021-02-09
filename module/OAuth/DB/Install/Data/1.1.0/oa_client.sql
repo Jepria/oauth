60419d31aa37ad13fa1d987d10540103668b9155
AlexSav94 <aleksoider@gmail.com>
2021-02-09 14:15:11 +0300

    fix: if clause removed

--- module/OAuth/DB/Install/Data/1.1.0/oa_client.sql
+++ module/OAuth/DB/Install/Data/1.1.0/oa_client.sql
@@ -1,33 +1,18 @@
 declare
 
   Client_SName constant varchar2(50) := 'OAuthClient';
-
   operatorId integer := pkg_Operator.getCurrentUserId();
-
-  foundFlag integer;
-
-  clientId integer;
   clientUriId integer;
 
 begin
-  select
-    count(*)
-  into foundFlag
-  from
-    oa_client cl
-  where
-    cl.client_short_name = Client_SName
-  ;
-  if foundFlag = 0 then
-    clientUriId := pkg_OAuth.createClientUri(
-      clientShortName => Client_SName
-      , clientUri => '/oauth-admin/oauth'
-      , operatorId => operatorId
-    );
+  clientUriId := pkg_OAuth.createClientUri(
+    clientShortName => Client_SName
+    , clientUri => '/oauth-admin/oauth'
+    , operatorId => operatorId
+  );
     dbms_output.put_line(
-      'client created: ' || Client_SName
-      || ' (client_id=' || clientId || ')'
+      'client uri created: ' || Client_SName
+      || ' (client_uri_id=' || clientUriId || ')'
     );
-  end if;
 end;
 /

7502cd37ae37fb56dd3aa80dc28b3218ba8fb52d
AlexSav94 <aleksoider@gmail.com>
2021-03-11 18:34:28 +0300

    revert: previous commit

--- module/OAuth/DB/Install/Schema/Last/ReserveDb/MViewSchema/mv_oa_key.sql
+++ module/OAuth/DB/Install/Schema/Last/ReserveDb/MViewSchema/mv_oa_key.sql
@@ -1,1 +1,1 @@
-@oms-run ./oms-create-mview.sql mv_oa_key Install/Schema/Last/ReserveDb/MViewSchema/mv_oa_key.snp
+@oms-run ./oms-recreate-mview.sql mv_oa_key Install/Schema/Last/ReserveDb/MViewSchema/mv_oa_key.snp
